83e1290328403e43dbc2c910ad7d3e0bb31ff685
Ahmad Einieh <66053897+ahmad-einieh@users.noreply.github.com>
2023-01-28 16:40:11 +0300

    finish up login

--- lib/style.dart
+++ lib/style.dart
@@ -5,3 +5,5 @@ var backgroundColor = const Color(0x01E1E1E);
 var textFormFileColor = const Color.fromRGBO(37, 37, 37, 1);
 
 var elevatedButtonColor = const Color.fromRGBO(59, 61, 191, 1);
+
+var appGrayColor = const Color.fromRGBO(161, 161, 161, 1);
