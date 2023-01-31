e0cad1afee3f7831c300067139a01c8012e85cfe
Ahmad Einieh <66053897+ahmad-einieh@users.noreply.github.com>
2023-01-31 22:20:50 +0300

    finish up something

--- lib/view/addElementPage.dart
+++ lib/view/addElementPage.dart
@@ -1,22 +1,33 @@
 import 'package:budget_tracker/controllers/addElementController.dart';
+import 'package:dropdown_button2/dropdown_button2.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_datepicker/datepicker.dart';
-import '../models/elementModel.dart';
 
 import '../constants.dart';
 import '../style.dart';
 import 'widgets/customInputFiled.dart';
 
-class AddElementPage extends StatelessWidget {
-  AddElementPage({super.key});
+class AddElementPage extends StatefulWidget {
+  const AddElementPage({super.key});
+
+  @override
+  State<AddElementPage> createState() => _AddElementPageState();
+}
+
+class _AddElementPageState extends State<AddElementPage> {
   TextEditingController elementName = TextEditingController();
+
   TextEditingController elementPrice = TextEditingController();
-  TextEditingController elementType = TextEditingController();
+
   DateRangePickerController elementDate = DateRangePickerController();
+
+  var elementTypes = ["طعام", "مواصلات", "مستلزمات", "فواتير", "تسوق", "دخل"];
 
   @override
   Widget build(BuildContext context) {
+    String? selectedValue;
+
     return SafeArea(
       child: Scaffold(
         backgroundColor: backgroundColor,
@@ -62,7 +73,6 @@ class AddElementPage extends StatelessWidget {
                   backgroundColor: calenderBlackColor,
                 ),
                 backgroundColor: calenderBlackColor,
-                onSelectionChanged: (v) {},
                 selectionMode: DateRangePickerSelectionMode.single,
                 monthCellStyle: const DateRangePickerMonthCellStyle(
                   textStyle: TextStyle(color: Colors.white),
@@ -93,35 +103,112 @@ class AddElementPage extends StatelessWidget {
                 hintText: "ادخل سعر العنصر",
                 inputType: InputType.number,
               ),
-              CustomInputFiled(
-                userController: elementType,
-                labelName: "النوع",
-                hintText: "ادخل النوع",
-                inputType: InputType.normal,
+              Padding(
+                padding: const EdgeInsets.only(top: 16),
+                child: DropdownButtonHideUnderline(
+                  child: DropdownButton2(
+                    isExpanded: true,
+                    hint: Row(
+                      children: const [
+                        Icon(
+                          Icons.list,
+                          size: 16,
+                          color: Colors.white,
+                        ),
+                        SizedBox(
+                          width: 4,
+                        ),
+                        Expanded(
+                          child: Text(
+                            '                     اختر النوع',
+                            style: TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ),
+                      ],
+                    ),
+                    items: elementTypes
+                        .map((item) => DropdownMenuItem<String>(
+                              value: item,
+                              child: Text(
+                                item,
+                                style: const TextStyle(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                ),
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ))
+                        .toList(),
+                    value: selectedValue,
+                    onChanged: (value) {
+                      setState(() {
+                        selectedValue = value;
+                      });
+                    },
+                    icon: const Icon(
+                      Icons.arrow_forward_ios_outlined,
+                    ),
+                    iconSize: 14,
+                    iconEnabledColor: Colors.white,
+                    iconDisabledColor: Colors.grey,
+                    buttonHeight: 66,
+                    buttonWidth: 300,
+                    buttonPadding: const EdgeInsets.only(left: 14, right: 14),
+                    buttonDecoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(14),
+                      border: Border.all(
+                        color: Colors.black26,
+                      ),
+                      color: elevatedButtonColor,
+                    ),
+                    buttonElevation: 2,
+                    itemHeight: 40,
+                    itemPadding: const EdgeInsets.only(left: 14, right: 14),
+                    dropdownMaxHeight: 200,
+                    dropdownWidth: 200,
+                    dropdownPadding: null,
+                    dropdownDecoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(14),
+                      color: elevatedButtonColor,
+                    ),
+                    dropdownElevation: 8,
+                    scrollbarRadius: const Radius.circular(40),
+                    scrollbarThickness: 6,
+                    scrollbarAlwaysShow: true,
+                    offset: const Offset(-20, 0),
+                  ),
+                ),
               ),
               Padding(
-                padding: const EdgeInsets.only(top: 44, left: 32, right: 32),
+                padding: const EdgeInsets.only(
+                    left: 32, right: 32, bottom: 20, top: 22),
                 child: SizedBox(
                   height: 66,
                   width: double.infinity,
                   child: ElevatedButton(
                     onPressed: () {
                       // print(elementDate.selectedDate);
-                      if (elementName.text.isEmpty ||
-                          elementPrice.text.isEmpty ||
-                          elementType.text.isEmpty) {
+                      if (elementName.text.isNotEmpty ||
+                          elementPrice.text.isNotEmpty ||
+                          selectedValue!.isNotEmpty ||
+                          selectedValue != null) {
                         addElement(
                             "elements",
                             elementName.text,
                             num.parse(elementPrice.text),
-                            elementType.text,
+                            selectedValue!,
                             elementDate.selectedDate ?? DateTime.now());
                         if (kDebugMode) {
                           print("done");
                         }
                         elementName.clear();
                         elementPrice.clear();
-                        elementType.clear();
                       }
                     },
                     style: ButtonStyle(
@@ -138,9 +225,9 @@ class AddElementPage extends StatelessWidget {
                   ),
                 ),
               ),
-              SizedBox(
-                height: MediaQuery.of(context).size.height * 0.05,
-              ),
+              // SizedBox(
+              //   height: MediaQuery.of(context).size.height * 0.05,
+              // ),
             ],
           ),
         ),
